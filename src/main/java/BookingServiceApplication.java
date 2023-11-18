ee8f3c1c3b319da54317f320a13be0609e2e473b
Jorge Cellerino <ncellerino@gmail.com>
2023-11-17 21:05:51 -0300

    initializing spring service

--- src/main/java/BookingServiceApplication.java
+++ src/main/java/BookingServiceApplication.java
@@ -1,5 +1,3 @@
-package com.hostfully;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
